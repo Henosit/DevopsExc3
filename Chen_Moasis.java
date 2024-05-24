31202eb311d43ea716e7d9232084efbf98909e9e
Henosit <chenmoasis@gmail.com>
2024-05-24 16:12:51 +0300

    Changed Chen_Moasis.java

--- Chen_Moasis.java
+++ Chen_Moasis.java
@@ -1,7 +1,7 @@
 public class Chen_Moasis{
 public static void main(String[] args) {
 for (int i=1;i<=10; i++) {
-System.out.println("Chen Moasis"+i);
+System.out.println("My name is Chen Moasis"+i);
 }
 }
 }
